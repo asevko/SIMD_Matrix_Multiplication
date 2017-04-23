c2e1e1eda88fe0d0017801b07b37f28ff88b7cc6
lesha <roksqq@yandex.ru>
2017-04-24 00:11:52 +0300

    time & graph

--- generation.js
+++ generation.js
@@ -1,67 +1,222 @@
 /**
  * Created by Лёша on 23.04.2017.
  */
+const LOW_LIMIT = -1;
+const HIGH_LIMIT = 1;
+let _P, _M, _Q;
+let A = [], B = [], C = [];
+let parallel_time = {
+    t_s: 1,
+    t_r: 1,
+    t_y: 1,
+    t_d: 1,
+    t_c: 1,
+    t_m: 1
+};
+let consecutive_time = {
+    t_s: 1,
+    t_r: 1,
+    t_y: 1,
+    t_d: 1,
+    t_c: 1,
+    t_m: 1,
+    t_all: 6
+};
+let statement = {
+    time_c: 1,
+    time: 1,
+    n: 1,
+    r: 1,
+    Ky_r: 1
+};
+let params = function() {
+   return {
+       p_size: getInputParameter("size_p"),
+       m_size: getInputParameter("size_m"),
+       q_size: getInputParameter("size_q"),
+       t_s: getInputParameter("t_s"),
+       t_r: getInputParameter("t_r"),
+       t_y: getInputParameter("t_y"),
+       t_d: getInputParameter("t_d"),
+       t_c: getInputParameter("t_c"),
+       t_m: getInputParameter("t_m")
+   };
+};
+
+function run() {
+    if (document.getElementById("output").innerHTML !== "") {
+        document.getElementById("output").innerHTML = "";
+    }
+    let parameters = params();
+    _M = parameters.m_size;
+    _Q = parameters.q_size;
+    _P = parameters.p_size;
+    parallel_time.t_s = parameters.t_s;
+    parallel_time.t_r = parameters.t_r;
+    parallel_time.t_y = parameters.t_y;
+    parallel_time.t_d = parameters.t_d;
+    parallel_time.t_c = parameters.t_c;
+    parallel_time.t_m = parameters.t_m;
+    consecutive_time.t_s = parameters.t_s;
+    consecutive_time.t_r = parameters.t_r;
+    consecutive_time.t_y = parameters.t_y;
+    consecutive_time.t_d = parameters.t_d;
+    consecutive_time.t_c = parameters.t_c;
+    consecutive_time.t_m = parameters.t_m;
+    consecutive_time.t_all = 6;
+    call();
+}
+
+function clean() {
+    document.getElementById("output").innerHTML = "";
+    document.getElementById("size_p").value = "";
+    document.getElementById("size_m").value = "";
+    document.getElementById("size_q").value = "";
+    document.getElementById("t_s").value = "";
+    document.getElementById("t_r").value = "";
+    document.getElementById("t_y").value = "";
+    document.getElementById("t_d").value = "";
+    document.getElementById("t_c").value = "";
+    document.getElementById("t_m").value = "";
+}
+
+function getInputParameter(id) {
+    if (+document.getElementById(id).value < 1 ||
+        typeof +document.getElementById(id).value != "number")
+        throw new Error("Bad types");
+    return parseInt(document.getElementById(id).value);
+}
+
 function call() {
-    var output = document.getElementById("output");
-    const LOW_LIMIT = -1;
-    const HIGH_LIMIT = 1;
-    var _P = 7, _M = 3, _Q = 7;
-    var A = [], B = [], C =[];
-    var isNegative = function() {
+    let output = document.getElementById("output");
+    let isNegative = function () {
         return Math.floor(Math.random() * 2) == 1;
     };
 
-    var randomFloat = function(){
+    let randomFloat = function () {
         return parseFloat(LOW_LIMIT * Math.random().toFixed(4) * (isNegative() ? LOW_LIMIT : HIGH_LIMIT));
     };
 
     function createMatrix(row, col) {
-        var array = [];
-        for (var i = 0; i < row; i++) {
+        let array = [];
+        for (let i = 0; i < row; i++) {
             array[i] = [];
-            for (var j = 0; j < col; j++)
+            for (let j = 0; j < col; j++)
                 array[i][j] = randomFloat();
         }
         return array;
     }
 
     function showMatrix(array, row, col) {
-        for (var i = 0; i < row; i++) {
-            for (var j = 0; j < col; j++)
+        for (let i = 0; i < row; i++) {
+            for (let j = 0; j < col; j++) {
+                // if(parseInt(array[i][j]) < 10)
+                //     output.innerHTML += "&nbsp;";
                 output.innerHTML += (array[i][j] + " ");
+            }
             output.innerHTML += "<br />";
         }
     }
     A = createMatrix(_P, _M);
     B = createMatrix(_M, _Q);
-    showMatrix(A, _P, _M);
-    output.innerHTML += "<br />";
-    output.innerHTML += "<br />";
-    showMatrix(B, _M, _Q);
-    output.innerHTML += "<br />";
-    output.innerHTML += "<br />";
+
     /**
      * @return {number}
      */
-    var SUM = function (i, j) {
-        var sum = 0;
-        for (var k = 0; k < _M; k++){
-            sum += (Math.pow(A[i][k], 2) > Math.abs(A[i][k] * B[k][j])) ?
-                Math.pow(A[i][k], 2) - Math.abs(A[i][k] * B[k][j]) :
-                (B[k][j] == 0 ?
-                    Math.pow(A[i][k], 2) :
-                    Math.pow(A[i][k], 2) / Math.abs(B[k][j]));
+    let SUM = function (i, j) {
+        let sum = 0;
+        for (let k = 0; k < _M; k++) {
+            // sum += (Math.pow(A[i][k], 2) > Math.abs(A[i][k] * B[k][j])) ?
+            //     Math.pow(A[i][k], 2) - Math.abs(A[i][k] * B[k][j]) :
+            //     (B[k][j] == 0 ?
+            //         Math.pow(A[i][k], 2) :
+            //         Math.pow(A[i][k], 2) / Math.abs(B[k][j]));
+            let a = Math.pow(A[i][k], 2);
+            let b = Math.abs(A[i][k] * B[k][j]);
+            consecutive_time.t_y += 2;
+            consecutive_time.t_m++;
+            consecutive_time.t_c++;
+            consecutive_time.t_s++;
+            if (a > b) {
+                consecutive_time.t_r++;
+                sum += a - b;
+            }
+            else {
+                consecutive_time.t_c++;
+                if (B[k][j] == 0) {
+                    sum += a;
+                }
+                else {
+                    consecutive_time.t_m++;
+                    consecutive_time.t_d++;
+                    sum += a / Math.abs(B[k][j]);
+                }
+            }
         }
         return sum;
     };
 
-    var createMatrix_C = function(_P, _Q) {
-        for (var i = 0; i < _P; i++) {
+    let createMatrix_C = function (_P, _Q) {
+        for (let i = 0; i < _P; i++) {
             C[i] = [];
-            for (var j = 0; j < _Q; j++)
-                C[i][j] = SUM(i,j).toFixed(4);
+            for (let j = 0; j < _Q; j++) {
+                C[i][j] = SUM(i, j).toFixed(4);
+            }
         }
     };
+
     createMatrix_C(_P, _Q);
     showMatrix(C, _P, _Q);
+    consecutive_time.t_all = consecutive_time.t_d + consecutive_time.t_y + consecutive_time.t_c
+        + consecutive_time.t_s + consecutive_time.t_r + consecutive_time.t_m;
+    console.log(consecutive_time);
+    statementGeneration();
+}
+
+function makeClone(obj) {
+    let clone = {};
+    for (let prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            if ("object"===typeof obj[prop])
+                clone[prop] = makeClone(obj[prop]);
+            else
+                clone[prop] = obj[prop];
+        }
+    }
+    return clone;
+}
+let statements = [];
+function statementGeneration(){
+    let time_s = document.getElementById("t_s").value;
+    let validProcessingElements = findDividers();
+    let n = 0;
+    validProcessingElements.forEach(function (element) {
+        statements[n] = [];
+        for (let i = 1; i <= _M + 5; i+= 2) {
+            let state = makeClone(statement);
+            let all_time = (consecutive_time.t_all + _P * _Q  *time_s);
+            for (let k = 1; k <= element; k++){
+                all_time+= all_time;
+            }
+            state.time_c = all_time * i;
+            state.time = all_time  + (i - 1) * time_s;
+            state.n = element;
+            state.r = i;
+            state.Ky_r =state.time_c / state.time;
+            statements[n].push(state);
+        }
+        n++;
+    });
+}
+
+let findDividers = function () {
+    let dividers = [];
+    for (let i = 1; i <= _M; i++)
+        if (_M % i == 0)
+            dividers.push(i);
+    return dividers;
+};
+
+function getStatements() {
+    return statements;
 }
